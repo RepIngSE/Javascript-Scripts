04c661cf28e0ad63c368faab36efe7581d82c8b3
Sgabi2604 <139818962+Sgabi2604@users.noreply.github.com>
2023-08-11 21:44:58 -0500

    Create

---
+++ Homework_Store.js
@@ -0,0 +1,8 @@
+function Tablas(){
+    var tienda = document.getElementById("tabla1"); 
+    for(var i = 0; i <= 5; i++){
+        var tiendaReplicada = tienda.cloneNode(true); 
+        tiendaReplicada.id = "tabla" + (i+2); 
+        document.body.appendChild(tablaReplicada); 
+    }
+}
